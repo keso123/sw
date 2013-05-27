227d9f32de86296cb6d50b7e7693f2e28db7a888
lvalerom <lvaleromartin@gmail.com>
2013-05-28 01:23:50 +0200

    DATE

--- SWcineDD/src/com/lvaleromsw/swcine/SearchServlet.java
+++ SWcineDD/src/com/lvaleromsw/swcine/SearchServlet.java
@@ -13,13 +13,24 @@ public class SearchServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		String redirect = "/index.jsp";
+		boolean err = false;
 		try{
 			String searchtext = request.getParameter("searchtext");
 			String type = request.getParameter("stype");
 			//request.setAttribute("stype",type);
 			
-			System.out.println(type);
-			redirect = "/searchs.jsp?stype="+type+"&s="+searchtext;
+			//System.out.println(type);
+			
+			if(searchtext == null){
+				err = true;
+			}
+			if(type == null){
+				err = true;
+			}
+			if((type.equals("all") || type.equals("title") || type.equals("director") || type.equals("cast"))
+					&& err == false){
+				redirect = "/searchs.jsp?stype="+type+"&s="+searchtext;
+			}
 			
 		}catch(Exception e){
 			
